868300bd9adbc6b8b4395139093e75bfa08bd6ec
tbone1020 <trentteebken@trents-mbp.home>
2019-03-29 00:22:26 -0700

    built the left side of the footer

--- src/app/footer/footer-image/footer-image.component.ts
+++ src/app/footer/footer-image/footer-image.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-footer-image',
   templateUrl: './footer-image.component.html',
-  styleUrls: ['./footer-image.component.css']
+  styleUrls: ['./footer-image.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FooterImageComponent implements OnInit {
 
