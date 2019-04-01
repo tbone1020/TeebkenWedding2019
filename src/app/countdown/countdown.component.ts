75375b979d3a33bc41ddbda8b3af84ff38983a55
tbone1020 <trentteebken@trents-mbp.home>
2019-03-31 22:36:57 -0700

    Started building countdown functionality

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountdownComponent implements OnInit {
 
-  constructor() { }
+  private weddingDate: Date = new Date("May, 26, 2019");
+  private todaysDate: Date = new Date();
+
+  constructor() {}
 
   ngOnInit() {
+    this.setMonthsTillWedding();
+  }
+  
+  setMonthsTillWedding(): void {
+    console.log(`Today's date ${this.todaysDate.getMonth() + 1}`);
+    console.log(`Wedding's date ${this.weddingDate.getMonth() + 1}`);
+  }
+
+  setDaysTillWedding(): any {
+
+  }
+  
+  setHoursTillWedding(): any {
+
+  }
+
+  setMinutesTillWedding(): any {
+
+  }
+
+  setSecondsTillWedding(): any {
+
   }
 
 }
